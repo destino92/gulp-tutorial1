8b9b0237f9b01bd9e6cb3963c73b048758f610bd
destino92 <destinodello@gmail.com>
2016-03-24 18:05:31 +0300

    created styles task with gulp-clean-css plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	cleanCSS = require('gulp-clean-css');
 
 gulp.task('scripts', function(){
-	gulp.src('js/*.js')
+	return gulp.src('js/*.js')
 		.pipe(uglify())
 		.pipe(rename('app.min.js'))
 		.pipe(gulp.dest('js/'));
+});
+
+gulp.task('styles', function(){
+	return gulp.src('css/*.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('minCSS'));
 });
 
